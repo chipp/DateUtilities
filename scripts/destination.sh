397d3baa3274701d844cfc79d80a8ebfa3891e3e
Vladimir Burdukov <chippcheg@gmail.com>
2019-01-16 11:43:08 +0300

    Fixed compatibility with Xcode 10.1

--- scripts/destination.sh
+++ scripts/destination.sh
@@ -1,6 +1,6 @@
 case $1 in
-  ("iOSTests") SIMULATOR='NetcoTests/iPhone-7/iOS/11.4' ;;
-  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/11.4' ;;
+  ("iOSTests") SIMULATOR='NetcoTests/iPhone-8/iOS/12.1' ;;
+  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/12.1' ;;
   ("macOSTests") echo "platform=macOS,arch=x86_64"; exit 0 ;;
   (*) exit -1 ;;
 esac
