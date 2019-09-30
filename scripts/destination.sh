dde9f7151fec98ce55f3473e8e2790154b15b01a
Vladimir Burdukov <chippcheg@gmail.com>
2019-09-30 18:51:49 +0300

    Updated required simulator versions for tests workflow

--- scripts/destination.sh
+++ scripts/destination.sh
@@ -1,6 +1,6 @@
 case $1 in
-  ("iOSTests") SIMULATOR='NetcoTests/iPhone-8/iOS/12.2' ;;
-  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/12.2' ;;
+  ("iOSTests") SIMULATOR='NetcoTests/iPhone-8/iOS/12.4' ;;
+  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/12.4' ;;
   ("macOSTests") echo "platform=macOS,arch=x86_64"; exit 0 ;;
   (*) exit -1 ;;
 esac
